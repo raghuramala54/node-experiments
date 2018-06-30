9fa55f97f77c6df8c8955d171b900e4aa4689f55
raghuramala54 <raghuram.ala54@gmail.com>
2018-06-30 07:36:59 +0530

    starting out a new feature

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+function new_feature(){
+	console.log("trying out new feature");
+}
 function something(){
 	console.log("hey wats up");
 }
 console.log("hello world");
 something();
+new_feature();
